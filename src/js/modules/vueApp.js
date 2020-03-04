ddade17e125e64c323394d74c4252f20e663dc30
yandrievskiy <yandrievskiy@justcoded.co>
2020-03-04 18:05:25 +0200

    step 1

--- src/js/modules/vueApp.js
+++ src/js/modules/vueApp.js
@@ -2,15 +2,29 @@
 //
 // You can remove or add your own function in this file.
 
+import data from './data';
+
 const vueApp = () => {
   if (Vue) {
     new Vue({
       el: '#app',
-      template: '<div class="vue-app-wrapper" >{{ initMessage }}</div>',
+      template: `<div class="object-card-wrapper"><div class="object-card" v-for="object in list">
+        <img :src="object.imgUrl">
+        <p class="object-card__title">{{object.title}}</p>
+      </div></div>`,
       data() {
         return {
           initMessage: 'VueJS Sucessfully Started',
+          objects: [],
         };
+      },
+      mounted() {
+        this.objects = data;
+      },
+      computed: {
+        list() {
+          return this.objects.filter((item) => item.status === 'published' )
+        }
       },
     });
   }
